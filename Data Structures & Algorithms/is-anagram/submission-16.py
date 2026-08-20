c3cfa127b2099e6799f3394dfec30ae9759df584
YASH PAL <126085548+yashpalsince2004@users.noreply.github.com>
2026-08-21 01:00:54 +0530

    Add: is-anagram - submission-16

---
+++ Data Structures & Algorithms/is-anagram/submission-16.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        for ch in t:
+            if ch not in count:
+                return False
+
+            count[ch] -= 1
+
+            if count[ch] < 0:
+                return False
+
+        return True
